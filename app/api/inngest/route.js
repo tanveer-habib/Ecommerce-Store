8e69e5165040e70e905605deecffb3e2a700fc8e
tanveer-habib <92tanveer94478@gmail.com>
2026-02-25 06:43:37 +0500

    first commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,11 @@
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/inngest/functions.js";
+
+export const { GET, POST, PUT } = serve({
+    client: inngest,
+    functions: [
+        syncUserCreation,
+        syncUserUpdation,
+        syncUserDeletion
+    ],
+});
